f8a1205351b33d6228d865a7d59cdf57ebb19d95
Juniper Watson <lyravox1@gmail.com>
2024-04-22 16:34:47 -0500

    Add information.py

---
+++ bot/cogs/information.py
@@ -0,0 +1,15 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Interaction, Member, SlashOption
+
+class Information(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @nextcord.slash_command(description="Returns bot latency")
+    async def ping(self, interaction: Interaction):
+        latency = self.bot.latency * 1000
+        await interaction.response.send_message(f"Pong! My latency is {latency}ms.")
+        
+def setup(bot):
+    bot.add_cog(Information(bot))
